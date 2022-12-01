cbcaaa3aabb211b83d24e487b3a2d676c5da287f
starwort <tcphone93@gmail.com>
2022-12-01 19:16:02 +0000

    Restore old solution (suppress unused_variables lint

--- Rust/src/solutions/day_01.rs
+++ Rust/src/solutions/day_01.rs
@@ -2,6 +2,7 @@ use crate::into_answers::IntoAnswers;
 
 #[rustfmt::skip]
 pub fn solution(input: String) -> impl IntoAnswers {
+    let solution = |i: String| {let mut y:Vec<i32>=i.split("\n\n").map(|g|g.split('\n').map(|i|->i32{i.parse().unwrap()}).sum()).collect();y.sort_by_key(|y|-y);(y[0],y[0]+y[1]+y[2])};
     // Solution score:     147 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     let solution = |i: String| {let mut y:Vec<i32>=i.split("\n\n").map(|g|g.lines().map(|x|x.parse::<i32>().unwrap()).sum()).collect();y.sort_by_key(|y|-y);(y[0],y[0]+y[1]+y[2])};
     solution(input)
