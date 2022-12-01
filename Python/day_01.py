2cd68db0eb85460a642eb602390ab7aa17af7b24
starwort <tcphone93@gmail.com>
2022-12-01 09:01:26 +0000

    Make it clear how scoring works

--- Python/day_01.py
+++ Python/day_01.py
@@ -3,5 +3,6 @@ solution = lambda i: (v:=sorted(sum(map(int,y.split('\n')))for y in i.split("\n\
 solution = lambda i: ((v:=sorted(sum(map(int,y.split()))for y in i.split("\n\n")))[-1],sum(v[-3:]))
 # the following solution was created before solutions were requred to return an in-order tuple
 solution = lambda i: (sum(v:=sorted(sum(map(int,y.split()))for y in i.split('\n\n'))[-3:]),v[2])[::-1]
+# score:          77 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 solution = lambda i: (sum(v:=sorted(eval(i.replace('\n','+').replace('++',",")))[-3:]),v[2])[::-1]
 # fmt: on
--- Rust/src/solutions/day_01.rs
+++ Rust/src/solutions/day_01.rs
@@ -2,6 +2,7 @@ use crate::into_answers::IntoAnswers;
 
 #[rustfmt::skip]
 pub fn solution(input: String) -> impl IntoAnswers {
+    // Solution score:     151 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     let solution = |i: String| {let mut y:Vec<i32>=i.split("\n\n").map(|g|g.split('\n').map(|i|->i32{i.parse().unwrap()}).sum()).collect();y.sort_by_key(|y|-y);(y[0],y[0]+y[1]+y[2])};
     solution(input)
 }
