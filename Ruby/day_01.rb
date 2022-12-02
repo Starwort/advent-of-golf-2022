fd55c5ad242d6b19f3e3dcb984db35b47561d32a
fig <fig@users.noreply.github.com>
2022-12-02 12:30:27 +0000

    Ruby 01 82 -> 64

--- Ruby/day_01.rb
+++ Ruby/day_01.rb
@@ -1,2 +1,4 @@
 # u/Vanerac: 82c        vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 $solution = lambda { |i| [(e=i.split("\n\n").map{|l|l.split("\n").map(&:to_i).sum}.sort)[-1],e[-3..-1].sum] }
+# u/442401: 64c          vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+$solution = lambda { |i| [(e=i.split("\n\n").map{_1.split.sum(&:to_i)}).max,e.max(3).sum] }
