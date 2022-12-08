5788261bcf8380883d56a4d02af1a84ccacc2534
starwort <tcphone93@gmail.com>
2022-12-08 13:51:01 +0000

    Rename Bash folder and clarify its rules

--- bash/day2.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# 185 characters
-declare -A x=([AX]=43 [AY]=84 [AZ]=38 [BX]=11 [BY]=55 [BZ]=99 [CX]=72 [CY]=26 [CZ]=67);f()(return ${x[$c$d]:$1:($1+1)};);while read c d;do f 0;a=$[a+$?];f 1;b=$[b+$?];done<$1;echo $a $b
