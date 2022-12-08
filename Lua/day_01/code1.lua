a64852dd172ebe8c16d29b9bd688b52c02e1121e
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-09 07:47:28 +0800

    apply rule changes

--- Lua/day_01/code1.lua
+++ Lua/day_01/code1.lua
@@ -1,2 +1,2 @@
--- 127
-s,e=load("return{"..io.read"*a":gsub("\n","+"):gsub("+%+",","):sub(1,-2).."}")()table.sort(s)g=#s print(s[g],s[g-2]+s[g-1]+s[g])
+-- 136
+return function(i)s=load("return{"..i:gsub("\n","+"):gsub("+%+",","):sub(1,-2).."}")()table.sort(s)g=#s print(s[g],s[g-2]+s[g-1]+s[g])end
