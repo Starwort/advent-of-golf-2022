fb67675ac5af64cbe3176a6b289e8617c17c4189
Eric Blake <eblake@redhat.com>
2022-12-08 14:10:50 -0600

    M4 tighten IO rules
    Most m4 solutions were ambivalent to the presence or absence of a
    trailing newline in input; explicitly stating that it is absent costs
    one more byte in day 3's solution where the final newline was
    important, but makes M4 more uniform with other languages.

--- M4/runner.sh
+++ M4/runner.sh
@@ -1,32 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 if test $# -lt 1; then
     echo "Missing day argument" >&2; exit 2;
 fi
-# Some m4 solutions require trailing newline as part of the data stream;
-# the instructions say input will be in a file or macro 'i'.  Since
-# not all ../inputs/*.in include the trailing newline, this runner opts
-# for creating an actual file 'i'.
+# Some earlier m4 solutions required trailing newline as part of the data
+# stream; they have since been adjusted to directly use ../inputs/*.in that
+# excludes the trailing newline.  Use 'awk 1 <../inputs/$1.in > i' and drop
+# the -Di=... below to reproduce those solutions.
+rm -f expected actual i
 if test ! -f ../inputs/$1.in; then
     echo "No input file for day $1" >&2; exit 2;
 fi
-awk 1 < ../inputs/$1.in > i
 if test $# -eq 1; then
     if test ! -f day_$1.m4; then
         echo "No golfed submissions for entire day $1" >&2; exit 2;
     fi
-    { cat ../solutions/$1/1.solution; echo; \
-      cat ../solutions/$1/2.solution; echo; } | sed -n '/./p' > expected
-    { m4 day_$1.m4 || :; echo; } \
-        | tr -s '[:space:]' '\n' | sed -n '/./p' > actual
+    expected=$(cat ../solutions/$1/1.solution; printf '\n'; \
+               cat ../solutions/$1/2.solution)
+    actual=$(m4 -Di=../inputs/$1.in day_$1.m4)
 elif test $# -eq 2; then
     if test ! -f day_$1_$2.m4; then
         echo "No golfed submissions for day $1 part $2" >&2; exit 2;
     fi
-    { cat ../solutions/$1/$2.solution; echo; } | sed -n '/./p' > expected
-    { m4 day_$1_$2.m4 || :; echo; } \
-        | tr -s '[:space:]' '\n' | sed -n '/./p' > actual
+    expected=$(cat ../solutions/$1/$2.solution)
+    actual=$(m4 -Di=../inputs/$1.in day_$1_$2.m4)
 fi
-diff -u expected actual
-cat actual
+diff -u <(printf %s\\n "$expected") <(printf %s\\n "$actual" | tr ' ' '\n')
+printf %s\\n "$actual"
