c7b53dc47c7673db42eef43597f68f8a05561f3c
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-09 11:40:22 +0800

    update according to new rules

--- Lua/day_01/code1.lua
+++ Lua/day_01/code1.lua
@@ -1,2 +1,2 @@
 -- 136
-return function(i)s=load("return{"..i:gsub("\n","+"):gsub("+%+",","):sub(1,-2).."}")()table.sort(s)g=#s print(s[g],s[g-2]+s[g-1]+s[g])end
+return function(i)s=load("return{"..i:gsub("\n","+"):gsub("+%+",","):sub(1,-2).."}")()table.sort(s)g=#s return s[g],s[g-2]+s[g-1]+s[g]end
