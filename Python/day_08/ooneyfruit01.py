2d443bb2ef03c1ae2f298215e67a08c1bd51899f
Ooneyfruit <81097382+Ooneyfruit@users.noreply.github.com>
2022-12-11 09:37:21 +0000

    Create ooneyfruit01.py
    Based on https://github.com/Starwort/advent-of-golf-2022/blob/master/Python/day_08/pydis11.py
    Saves 2 bytes.

---
+++ Python/day_08/ooneyfruit01.py
@@ -0,0 +1,5 @@
+# 280
+# fmt: off
+l=*open(0),;s=c=0
+for j in range(9801):t=l[i:=j//99];j%=99;r=[*zip(*l)][j];s+=t[j]>min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[i+1:],r[:i][::-1]))));v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%u),map(len,u)));c=max(c,v*w*x*y)
+print(s,c)
